a3549663e4e978d5b1ffce19bad4f02e3041afb0
charlesdev <charleswealthinternational@gmail.com>
2021-08-21 03:39:54 -0700

    frontend login works good

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,39 @@
+import {Button} from 'react-bootstrap';
+
 function Register() 
 {
     return (
         <>
-            <h1>Register page</h1>
+            <h5>Register page</h5>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+
+                        <div className="card rounded m-2 p-5">
+
+                        <div className="input-group mb-5">
+                            <span className="input-group-text fa fa-user" id="basic-addon1"></span>
+                            <input type="text" className="form-control" placeholder="Name"/>
+                        </div>
+
+                        <div className="input-group mb-5">
+                            <span className="input-group-text fa fa-envelope" id="basic-addon1"></span>
+                            <input type="text" className="form-control" placeholder="E-mail"/>
+                        </div>
+
+                        <div className="input-group mb-5">
+                            <span className="input-group-text fa fa-unlock-alt" id="basic-addon1"></span>
+                            <input type="text" className="form-control" placeholder="password"/>
+                        </div>
+                        
+                        <Button className="btn btn-primary col-md-12">Submit</Button>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
